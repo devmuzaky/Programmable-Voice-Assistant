8734c00a1063e5df667a66cdbf71d0c23a374371
Devmu <devmuzaky@gmail.com>
2022-11-08 11:11:26 +0200

    resolving comment

--- src/app/recorder/components/recorder/recorder.component.ts
+++ src/app/recorder/components/recorder/recorder.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-recorder', templateUrl: './recorder.component.html', styleUrls: ['./recorder.component.scss']
+  selector: 'app-recorder',
+  templateUrl: './recorder.component.html',
+  styleUrls: ['./recorder.component.scss']
 })
 export class RecorderComponent implements OnInit {
   isRecording;
