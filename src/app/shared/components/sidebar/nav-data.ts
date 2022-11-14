bf3c78dab7146d4ee0f6e4ac695d4d5ef052f742
Devmu <devmuzaky@gmail.com>
2022-11-14 20:04:41 +0200

    create nav bar data file

---
+++ src/app/shared/components/sidebar/nav-data.ts
@@ -0,0 +1,17 @@
+export const navbarData = [
+  {
+    routeLink: 'home',
+    icon: './assets/icons/homepage.svg',
+    label: 'Home'
+  },
+  {
+    routeLink: 'recorder',
+    icon: './assets/icons/voice-recorder.svg',
+    label: 'Recorder'
+  },
+  {
+    routeLink: 'detail',
+    icon: './assets/icons/homepage.svg',
+    label: 'Details'
+  }
+];
