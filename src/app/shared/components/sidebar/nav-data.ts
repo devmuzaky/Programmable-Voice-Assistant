7467a66da8a3a184138f79683a709c4d6e376fb4
ahmedEid1 <ahmedeid6422@gmail.com>
2023-01-26 14:24:45 +0200

    remove unneeded files (home + details)

--- src/app/shared/components/sidebar/nav-data.ts
+++ src/app/shared/components/sidebar/nav-data.ts
@@ -1,18 +1,9 @@
 export const navbarData = [
-  {
-    routeLink: 'home',
-    icon: './assets/icons/homepage.svg',
-    label: 'Home'
-  },
+
   {
     routeLink: 'recorder',
     icon: './assets/icons/voice-recorder.svg',
     label: 'Recorder'
-  },
-  {
-    routeLink: 'detail',
-    icon: './assets/icons/homepage.svg',
-    label: 'Details'
   },
   {
     routeLink: 'my-scripts',
