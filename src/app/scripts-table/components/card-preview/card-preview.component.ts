b0e249ab44ec00679747746adac3be4eded9314e
Mohamed Zaky <devmuzaky@gmail.com>
2023-03-07 16:57:34 +0200

    create card preview component for marketplace

---
+++ src/app/scripts-table/components/card-preview/card-preview.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-card-preview',
+  templateUrl: './card-preview.component.html',
+  styleUrls: ['./card-preview.component.scss']
+})
+export class CardPreviewComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+}
+
