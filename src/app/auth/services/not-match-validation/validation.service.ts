bf9dc79400949111dffcd92f9999d5b33956f663
Moe Zaky <devmuzaky@gmail.com>
2023-03-19 19:54:09 +0200

    create validation service to match passwords

---
+++ src/app/auth/services/not-match-validation/validation.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {AbstractControl, ValidatorFn} from "@angular/forms";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidationService {
+
+  passwordMatch(password: string, confirmPassword: string): ValidatorFn {
+    return (formGroup: AbstractControl): { [key: string]: any } | null => {
+      const passwordControl = formGroup.get(password);
+      const confirmPasswordControl = formGroup.get(confirmPassword);
+
+      if (!passwordControl || !confirmPasswordControl) {
+        return null;
+      }
+
+      if (
+        confirmPasswordControl.errors &&
+        !confirmPasswordControl.errors.passwordMismatch
+      ) {
+        return null;
+      }
+
+      if (passwordControl.value !== confirmPasswordControl.value) {
+        confirmPasswordControl.setErrors({ passwordMismatch: true });
+        return { passwordMismatch: true };
+      } else {
+        confirmPasswordControl.setErrors(null);
+        return null;
+      }
+    };
+  }
+}
